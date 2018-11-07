ae8a1470c887d6bb40a1ab02b405185040005c6b
Bas Jacobs <basjacobs93@gmail.com>
2018-11-07 11:58:29 +0100

    Image creation and plotting

--- genetic.py
+++ genetic.py
@@ -1,15 +1,74 @@
-from typing import List, Float
+from typing import List
+from image import Image
+from numpy import argsort
 
-def fitness(Image: image) -> float:
+
+def create_initial_population() -> List[Image]:
+	"""
+	Create population at step 0
+	"""
 	pass
 
-def group_entities():
+
+def evaluate_population(images: List[Image]) -> List[Image]:
+	"""
+	Calculate fitness per image, return ordered
+	"""
+
+	scores = [fitness(image) for image in images]
+	for i in len(images):
+		image[i].fitness = scores[i]
+
+	# sort images in order of scores
+	scores, images = zip(*sorted(zip(scores, images)))
+	print(scores[0])
+
+	return images
+
+
+def fitness(image: Image) -> float:
+	"""
+	Calculate score per entity
+	"""
 	pass
 
-def crossover():
+
+def group_entities(images: List[Image]) -> List[List[Image]]:
+	"""
+	Take a population and return groups of
+	entities to be used in crossover
+	"""
 	pass
 
-def mutate():
+
+def crossover(group: List[Image]) -> Image:
+	"""
+	Take a group of parents and return a child
+	"""
 	pass
+
+
+def mutate(image: Image) -> Image:
+	"""
+	Take an image and return (with a certain probability)
+	a mutated version
+	"""
+	pass
+
+
+def runga():
+	"""
+	Main loop
+	"""
+	population = create_initial_population()
+
+	for _ in range(10):
+		pass
+
+
+
+if __name__ == "__main__":
+	runga()
+
 
 
--- image.py
+++ image.py
@@ -1,4 +1,56 @@
-from typing import NewType
+from typing import List
+from pandas import DataFrame
+from plotnine import *
 
-UserId = NewType('UserId', int)
-some_id = UserId(524313)
+class Shape(List):
+	def __init__(self, info: dict) -> None:
+		self.df = DataFrame(info)
+
+	def __print__(self) -> None:
+		print(self.df.values)
+
+class Rectangle(Shape):
+	def show(self) -> geom_rect:
+		return geom_rect(
+			data = self.df,
+			mapping = aes(xmin = "xmin", xmax = "xmax",
+						  ymin = "ymin", ymax = "ymax"),
+			fill = self.df.color)
+
+
+class Image(List):
+    def __init__(self, width: float, height: float,
+    			 elements: List[Shape]) -> None:
+    	self.elements = elements
+    	self.width = width
+    	self.height = height
+    	self.fitness = None
+    	pass
+
+    def show(self) -> None:
+    	plot = (ggplot() +
+				# make plot white, add border
+				theme(line = element_blank(),
+					  text = element_blank(),
+					  title = element_blank(),
+					  plot_background = element_blank(),
+					  panel_border = element_rect(colour = "black",
+					  							  fill = None, size = 1)) +
+    			xlim(0, self.width) + ylim(0, self.height))
+    	# add elements to plot
+    	for element in self.elements:
+    		plot += element.show()
+    	print(plot)
+
+    def __print__(self) -> None:
+    	for element in elements:
+    		print(element)
+    		print("-"*10)
+
+
+if __name__ == "__main__":
+	rect1 = Rectangle({"xmin": [2], "xmax": [3], "ymin": [2], "ymax": [3], "color": ["blue"]})
+	rect2 = Rectangle({"xmin": [4], "xmax": [7], "ymin": [5], "ymax": [6], "color": ["red"]})
+	print(rect1)
+	im = Image(10, 10, [rect1, rect2])
+	im.show()
